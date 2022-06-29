a02011486d92cd3d5116d9210d79cb35ea8f43de
Nikita <muratikovnikita@gmail.com>
2022-06-29 20:07:35 +0300

    2 домашняя работа

---
+++ Homework2 Seminar/zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+TripleNumber(number);
+
+ void TripleNumber(string currentNumber)
+{
+         if (currentNumber.Length < 3)
+            {
+                Console.WriteLine("Третьей цифры нет");
+            }
+            else if (currentNumber[2] != 0)
+            {
+                Console.WriteLine(currentNumber[2]);
+            }
+
+            
+}
---
+++ Homework2 Seminar/zadanie2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Callendare(number);
+void Callendare (int number)
+{
+if(number== 1) Console.WriteLine("Понедельник"); 
+else if(number == 2) Console.WriteLine("Вторник"); 
+else if(number == 3) Console.WriteLine("Среда"); 
+else if(number == 4) Console.WriteLine("Четверг"); 
+else if(number == 5) Console.WriteLine("Пятнтица"); 
+else if(number == 6) Console.WriteLine("Суббота"); 
+else if(number == 7) Console.WriteLine("Воскресение"); 
+}
+if(number==6 ||number==7 )
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("рабочий день");
+}
